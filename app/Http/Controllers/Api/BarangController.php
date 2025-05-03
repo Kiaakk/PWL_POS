368a4bac2e966aa3443c060bb5e6d5f04367097f
Kiaakk <krisna.wiliam26@gmail.com>
2025-05-03 11:23:43 +0700

    Minggu 10: Tugas

--- app/Http/Controllers/Api/BarangController.php
+++ app/Http/Controllers/Api/BarangController.php
@@ -1,41 +1,74 @@
 <?php
- 
- namespace App\Http\Controllers\Api;
- 
- use App\Http\Controllers\Controller;
- use Illuminate\Http\Request;
- use App\Models\BarangModel;
- 
- class BarangController extends Controller
- {
-     public function index()
-      {
-          return BarangModel::all();
-      }
-  
-      public function store(Request $request)
-      {
-          $barang = BarangModel::create($request->all());
-          return response()->json($barang, 201);
-      }
-  
-      public function show(BarangModel $barang)
-      {
-         //  return BarangModel::find($barang);
-         return response()->json($barang);
-      }
-      public function update(Request $request, BarangModel $barang)
-      {
-          $barang->update($request->all());
-          return $barang;
-      }
-  
-      public function destroy(BarangModel $barang)
-      {
-          $barang->delete();
-          return response()->json([
-              'success' => true,
-              'message' => 'Data berhasil dihapus'
-          ]);
-      }
- }
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\BarangModel;
+use Illuminate\Support\Facades\Validator;
+
+class BarangController extends Controller
+{
+    public function index()
+    {
+        return BarangModel::all();
+    }
+
+    public function store(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'kategori_id' => 'required',
+            'barang_kode' => 'required',
+            'barang_nama' => 'required',
+            'harga_beli' => 'required|numeric',
+            'harga_jual' => 'required|numeric',
+            'image' => 'required|image|mimes:jpg,jpeg,png|max:2048'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $image = $request->file('image');
+        $image->store('public/posts');
+
+        $barang = BarangModel::create([
+            'kategori_id' => $request->kategori_id,
+            'barang_kode' => $request->barang_kode,
+            'barang_nama' => $request->barang_nama,
+            'harga_beli' => $request->harga_beli,
+            'harga_jual' => $request->harga_jual,
+            'image' => $image->hashName()
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data berhasil disimpan',
+            'data' => $barang
+        ], 201);
+        
+        // $barang = BarangModel::create($request->all());
+        // return response()->json($barang, 201);
+    }
+
+    public function show(BarangModel $barang)
+    {
+        //  return BarangModel::find($barang);
+        return response()->json($barang);
+    }
+    public function update(Request $request, BarangModel $barang)
+    {
+        $barang->update($request->all());
+        return $barang;
+    }
+
+    public function destroy(BarangModel $barang)
+    {
+        $barang->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data berhasil dihapus'
+        ]);
+    }
+}
