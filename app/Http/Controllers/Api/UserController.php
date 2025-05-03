b20e3d20064c6bc2602884efa592972a80e50f6c
Kiaakk <krisna.wiliam26@gmail.com>
2025-05-03 10:42:30 +0700

    refactor: remove unused code in UserController

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -35,43 +35,4 @@
              'message' => 'Data berhasil dihapus'
          ]);
      }
-
-     public function __invoke(Request $request) {
-        $validator = Validator::make($request->all(), [
-            'username' => 'required',
-            'nama' => 'required',
-            'password' => 'required|min:5|confirmed',
-            'level_id' => 'required',
-            'foto' => 'required',
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
-        ]);
-
-        //if validations faild
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
-        }
-
-        //create user
-        $user = UserModel::create([
-            'username' => $request->username,
-            'nama'     => $request->nama,
-            'password' => bcrypt($request->password),
-            'level_id' => $request->level_id,
-            'foto'     => $request->foto,
-            'image'    => $request->image,
-        ]);
-
-        //return response JSON user is created
-        if ($user) {
-            return response()->json([
-                'success' => true,
-                'user'    => $user,
-            ], 201);
-        }
-
-        return response()->json([
-            'success' => false,
-        ], 409);
-     }
-     
  }
